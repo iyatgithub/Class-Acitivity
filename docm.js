3b8a97cde6baa6feea57c429513313a28fc03738
iyatgithub <163440576+iyatgithub@users.noreply.github.com>
2024-03-22 21:57:04 +0500

    Create docm.js

---
+++ docm.js
@@ -0,0 +1,6 @@
+function update(){
+    var input = document.getElementById('123')
+    console.log(input.value);
+    var output = document.getElementById('spa')
+    output.innerHTML = input.value
+}
